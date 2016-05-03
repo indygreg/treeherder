26c40a424b37a06d84ca149064f075115503e8e4
James Graham <james@hoppipolla.co.uk>
2016-05-03 22:15:18 +0100

    Try optimising es_import_failure_lines a little to use less memory

--- treeherder/model/management/commands/es_import_failure_lines.py
+++ treeherder/model/management/commands/es_import_failure_lines.py
@@ -26,22 +26,41 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        min_id = FailureLine.objects.order_by('id')[0].id - 1
+        min_id = FailureLine.objects.order_by('id').values_list("id", flat=True)[0] - 1
         print options
         chunk_size = options['chunk_size']
 
         while True:
-            rows = FailureLine.objects.filter(id__gt=min_id).order_by('id')[:chunk_size]
+            rows = (FailureLine.objects
+                    .filter(id__gt=min_id)
+                    .order_by('id')
+                    .values("id", "job_guid", "action", "test", "subtest",
+                            "status", "expected", "message", "best_classification_id",
+                            "best_is_verified"))[:chunk_size]
             if not rows:
                 break
             es_lines = []
             for item in rows:
-                es_line = TestFailureLine.from_model(item)
+                es_line = failure_line_from_value(item)
                 if es_line:
                     es_lines.append(es_line)
             print "Inserting %i rows" % len(es_lines)
             bulk_insert(es_lines)
-            min_id = rows[len(rows) - 1].id
+            min_id = rows[len(rows) - 1]["id"]
             time.sleep(options['sleep'])
         s = Search(doc_type=TestFailureLine).params(search_type="count")
         print "Index contains %i documents" % s.execute().hits.total
+
+
+def failure_line_from_value(line):
+    if line["action"] == "test_result":
+        rv = TestFailureLine(job_guid=line["job_guid"],
+                             test=line["test"],
+                             subtest=line["subtest"],
+                             status=line["status"],
+                             expected=line["expected"],
+                             message=line["message"],
+                             best_classification=line["best_classification_id"],
+                             best_is_verified=line["best_is_verified"])
+        rv.meta.id = line["id"]
+        return rv
